8c5ab4aef8886a0e3eaad7ea5a901543a74cbc60
Jiss Joy <jissjoy3950@gmail.com>
2020-11-01 03:11:56 +0530

    Initial Commit

---
+++ app/src/main/java/com/smartechbraintechnologies/medillah/EmailAuthBridgeActivity.java
@@ -0,0 +1,14 @@
+package com.smartechbraintechnologies.medillah;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EmailAuthBridgeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_email_auth_bridge);
+    }
+}
